3f1b411e1f246ec50e9d9e4e8b444b0f5cc0dc5e
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-07 22:48:35 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -8,7 +8,7 @@ segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111 ]
 @cocotb.test()
 async def test_7seg(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 1, units="ms")
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # reset
@@ -26,3 +26,14 @@ async def test_7seg(dut):
         # all bidirectionals are set to output
         assert dut.uio_oe == 0xFF
 
+ # reset
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    dut._log.info(f"check all segments")
+    # check all segments and roll over
+    for i in range(15):
+        dut._log.info("check segment {}".format(i))
+        await ClockCycles(dut.clk, 1000)
+        assert int(dut.segments.value) == segments[i]
