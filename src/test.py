afca84ab2bbda0535340234afdd414594bfd2e69
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-07 23:17:10 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111 ]
+segments = [ 0, 112, 63, 111, 121, 56, 48, 63 ]
 
 @cocotb.test()
 async def test_7seg(dut):
@@ -20,13 +20,13 @@ async def test_7seg(dut):
     dut.rst_n.value = 1
 
     # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
+    max_count = dut.ui_in.value << 8
     dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
     # check all segments and roll over
     for i in range(15):
         dut._log.info("check segment {}".format(i))
         await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+        assert int(dut.segments.value) == segments[i % 8]
 
         # all bidirectionals are set to output
         assert dut.uio_oe == 0xFF
@@ -38,10 +38,10 @@ async def test_7seg(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    max_count = dut.ui_in.value << 10
+    max_count = dut.ui_in.value << 8
     dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
     # check all segments and roll over
     for i in range(15):
         dut._log.info("check segment {}".format(i))
         await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+        assert int(dut.segments.value) == segments[i % 8]
